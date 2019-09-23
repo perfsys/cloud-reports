3da96a46c343a5f50227e13bd8e62c6a2b6da230
Svetlana Tovt <ms.tsp00@gmail.com>
2019-09-23 19:43:48 +0300

    Front and back is ready

--- backend/handler.js
+++ backend/handler.js
@@ -1,18 +1,45 @@
-'use strict';
+"use strict";
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient();
+const uuidv4 = require("uuid/v4");
+require('dotenv').config();
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+const TABLE_NAME = process.env.TABLE_NAME;
+
+module.exports.postReport = async (event, context, callback) => {
+  let item = JSON.parse(event.body);
+  item.id = uuidv4();
+  item.timestamp = new Date.now();
+
+  let params = {
+    TableName: TABLE_NAME,
+    Item: item
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  try {
+    const data = await db.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify("report created")
+    };
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports.getReports = async (event, context, callback) => {
+
+  let params = {
+    TableName: TABLE_NAME
+  };
+
+  try {
+    const data = await db.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from "vuelidate";
+import Antd from 'ant-design-vue';
 
+Vue.use(Vuelidate);
+Vue.use(Antd);
 Vue.config.productionTip = false
 
 new Vue({
