079e1110734e196f280ec36186e3342d601d5982
grobakidze <spmgeorges@gmail.com>
2020-06-03 11:39:10 +0300

    DynamoDB table dumps

--- functions/cronDataSerializer.js
+++ functions/cronDataSerializer.js
@@ -16,19 +16,53 @@ const putObject = params => new Promise(
   (resolve, reject) => S3.putObject(params, (err, data) => err ? reject(err) : resolve(data))
 );
 
-module.exports = async (event, context,callback) => {
-  const tablesArr = Object.keys(process.env)
-    .filter((varb) => varb.includes('TABLE_NAME'))
-    .map((tableName) => process.env[tableName]);
+const getStartOfTheUTCDayTimestamp = () => {
+  let timestamp = Date.now();
 
-  await Promise.all(tablesArr.map((TableName) => new Promise((resolve, reject) => {
-    scan({ TableName })
-      .then(({ Items }) => stringify(Items))
-      .then((csvStr) => putObject({
-        Bucket: process.env.TABLE_DUMPS_BUCKET_NAME,
-        Body: csvStr,
-        Key: `${TableName}-${Date.now()}.csv`,
-      }))
-      .then((data) => resolve(data))
-  })));
+  timestamp = new Date(timestamp).setHours(0);
+  timestamp = new Date(timestamp).setMinutes(0);
+  timestamp = new Date(timestamp).setSeconds(0);
+  timestamp = new Date(timestamp).setMilliseconds(0);
+
+  return timestamp;
+};
+
+const dumpTables = async (tables) => {
+  const [ TableName, ...unprocessedTables ] = tables;
+  const timestamp = getStartOfTheUTCDayTimestamp();
+  const { Items } = await scan({
+    TableName,
+    FilterExpression: 'id > :timestamp',
+    ExpressionAttributeValues: {
+      ':timestamp': timestamp,
+    },
+  });
+
+  Items.sort((a, b) => a.id < b.id ? 1 : - 1);
+
+  const itemsToDump = [Items[0]];
+  const csvStr = await stringify(itemsToDump);
+  
+  await putObject({
+    Bucket: process.env.TABLE_DUMPS_BUCKET_NAME,
+    Body: csvStr,
+    Key: `daily/${TableName}-latest.csv`,
+  });
+
+  const dayOfTheWeek = new Date().getDay();
+  if (dayOfTheWeek === 7) {
+    await putObject({
+      Bucket: process.env.TABLE_DUMPS_BUCKET_NAME,
+      Body: csvStr,
+      Key: `weekly/${TableName}-latest.csv`,
+    });
+  }
+
+  if (unprocessedTables.length) return dumpTables(unprocessedTables);
+};
+
+module.exports = async (event, context, callback) => {
+  const tableNames = process.env.TABLES_TO_DUMP.split(',');
+  
+  await dumpTables(tableNames);
 };
